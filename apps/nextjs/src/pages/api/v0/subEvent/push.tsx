b5d7a802b9201134399bd8757bf84dcc4843bee9
Daniel Mizerski <duelsik@gmail.com>
2023-07-17 21:12:28 +0200

    finish subEvent push route

--- apps/nextjs/src/pages/api/v0/subEvent/push.tsx
+++ apps/nextjs/src/pages/api/v0/subEvent/push.tsx
@@ -1,9 +1,8 @@
-import { randomUUID } from "crypto";
 import { type NextApiHandler } from "next";
 import { z } from "zod";
 
 import { requireIntegrationV0 } from "@acme/api/src/utils/v0/integration";
-import { getNewOrContinueSessionV0 } from "@acme/api/src/utils/v0/session";
+import { prisma, type Prisma } from "@acme/db";
 
 const newEventAndNewSubEventInput = z.object({
   session: z.object({
@@ -11,7 +10,7 @@ const newEventAndNewSubEventInput = z.object({
   }),
   event: z.object({
     name: z.string().min(1),
-    data: z.any(),
+    data: z.object({}),
   }),
   subEvent: z.object({
     name: z.string().min(1),
@@ -36,18 +35,55 @@ const subEventV0Push: NextApiHandler = async (req, res) => {
   const integration = await requireIntegrationV0(req, res);
   if (!integration) return res;
 
-  const session = await getNewOrContinueSessionV0(integration);
-  const { name } = integration.user;
-
-  const placeholder = {
-    event: {
-      uuid: randomUUID(),
-    },
-    subEvent: {
-      uuid: randomUUID(),
-    },
-  };
-  return res.status(200).send(placeholder);
+  try {
+    const input = await newEventAndNewSubEventInput.parseAsync(req.body);
+    const newEvent = await prisma.eventV0.create({
+      data: {
+        name: input.event.name,
+        data: input.event.data as Prisma.JsonObject,
+        ownerId: integration.user.id,
+        createdById: integration.id,
+        parentId: input.session.id,
+      },
+    });
+    const newSubEvent = await prisma.subEventV0.create({
+      data: {
+        name: input.subEvent.name,
+        data: input.subEvent.data as Prisma.JsonObject,
+        ownerId: integration.user.id,
+        createdById: integration.id,
+        parentId: newEvent.id,
+      },
+    });
+    return res.status(200).json({
+      event: {
+        id: newEvent.id,
+      },
+      subEvent: {
+        id: newSubEvent.id,
+      },
+    });
+  } catch (e) {
+    try {
+      const input = await existingEventAndNewSubEventInput.parseAsync(req.body);
+      const newSubEvent = await prisma.subEventV0.create({
+        data: {
+          name: input.subEvent.name,
+          data: input.subEvent.data as Prisma.JsonObject,
+          ownerId: integration.user.id,
+          createdById: integration.id,
+          parentId: input.event.id,
+        },
+      });
+      return res.status(200).json({
+        subEvent: {
+          id: newSubEvent.id,
+        },
+      });
+    } catch (e) {
+      return res.status(400).send("Bad data");
+    }
+  }
 };
 
 export default subEventV0Push;
